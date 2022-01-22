695010d129bbca74943a401674ee8bbfc13e0491
Mamad <mohammadtt77@yahoo.com>
2022-01-22 23:40:52 +0330

    end of season 3

---
+++ androw codes/1/app.js
@@ -0,0 +1,50 @@
+/*
+// first section
+const fs = require("fs");
+
+fs.writeFileSync("notes.txt", "My name is johnny sins .");
+
+//challenge
+fs.appendFileSync("notes.txt", "this is added by append");
+
+*/
+
+// second section
+/*
+const add = require("./utils.js");
+
+const sum = add(4, -2);
+
+console.log(sum);
+
+// challenge
+
+const getNotes = require("./notes");
+
+console.log(getNotes());
+
+*/
+
+// third section
+
+// cmd => npm init
+// // npm install validator
+
+// const validator = require("validator");
+
+// console.log(validator.isEmail("test@example.com"));
+// console.log(validator.isEmail("example.com"));
+// console.log(validator.isURL("https://apple.com"));
+// console.log(validator.isURL("htps://apple.com"));
+
+// if node_modules removed => npm install
+
+// import chalk from "chalk";
+
+// const { chalk } = require("chalk"); *
+
+// import { chalk } from "chalk";
+
+// console.log(chalk.green("Success!"));
+
+console.log("aaasdasd ");
---
+++ androw codes/1/utils.js
@@ -0,0 +1,10 @@
+console.log("utils.js");
+
+const name = "Aleta";
+
+const add = (a, b) => {
+  return a + b;
+};
+
+// module.exports = name;
+module.exports = add;
