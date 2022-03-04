a2d83ed58ecd1ab119fc3ca1a0389cdf8259f9d2
Mamad <mohammadtt77@yahoo.com>
2022-03-05 01:41:47 +0330

    init mongoDB

---
+++ androw codes/4/mongodb.js
@@ -0,0 +1,7 @@
+// CRUD Create Read Update Delete
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+//   127.0.0.1 is localHost
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
