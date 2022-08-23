3f014d2761e8712646d380bbd48b7357baaa2353
mamad <mohammadtt77@yahoo.com>
2022-08-23 20:39:28 +0430

    finish review on node js

--- NODEJS REVIEW/2/web-server/public/js/app.js
+++ NODEJS REVIEW/2/web-server/public/js/app.js
@@ -1,1 +1,31 @@
-console.log("sex");
+fetch("http://puzzle.mead.io/puzzle").then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
+const form = document.querySelector("form");
+const search = document.querySelector("input");
+const h4 = document.querySelector("h4");
+console.log(form);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(search.value);
+  h4.innerText = "loading";
+  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
+    (response) => {
+      response.json().then((data) => {
+        console.log(data);
+        if (data.error) {
+          h4.textContent = data.error;
+        } else {
+          console.log(data.timezone);
+          console.log(data.weather);
+          console.log(data.forcast);
+          search.value = "";
+          h4.innerText =
+            data.timezone + " " + data.weather + " " + data.forcast;
+        }
+      });
+    }
+  );
+});
--- NODEJS REVIEW/2/web-server/src/app.js
+++ NODEJS REVIEW/2/web-server/src/app.js
@@ -1,33 +1,110 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
+const { forcast, geocode } = require("./utils/geocode");
+const { send } = require("process");
 
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
 
 const app = express();
 const publicDir = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../src/templates");
+const viewsPath = path.join(__dirname, "../src/templates/views");
+const partialsPath = path.join(__dirname, "../src/templates/partials");
+/////////////////////////////
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 app.use(express.static(publicDir));
+hbs.registerPartials(partialsPath);
+//////////////////////////////
 app.get("", (req, res) => {
-    res.render("index", {
-        title: "Weather App",
-        name: "Mamad",
+  res.render("index", {
+    title: "Weather App",
+    name: "Mamad",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send("Error you should add an address");
+  }
+  geocode(req.query.address, (error, data) => {
+    console.log("DATAAAAAAAAAAAAAAAAA");
+    // console.log(data);
+    if (error) {
+      return res.send(error);
+    }
+
+    // const dick = data.body ?? "pussy";
+    // console.log(dick);
+    // lat = data.body.length && data.body[0].lat ? data.body[0].lat : 0;
+    // lon = data.body.length && data.body[0].lon ? data.body[0].lon : 0;
+    let lat = 0;
+    let lon = 0;
+
+    if (data.body.length && data.body[0].lat && data.body[0].lon) {
+      lat = data.body[0].lat;
+      lon = data.body[0].lon;
+    }
+    if (!lat && !lon) {
+      res.status(502);
+      return res.send({
+        error: "error 502",
+      });
+    }
+    forcast(lat, lon, (error, forcastData) => {
+      if (error) {
+        return res.send(error);
+      }
+      res.send({
+        // forcast: forcastData,
+        timezone: forcastData.timezone,
+        weather: forcastData.current.weather[0].main,
+        forcast: forcastData.current.weather[0].description,
+
+        // description: forcastData.weather[0].description,
+      });
     });
+  });
+});
+
+app.get("/products", (req, res) => {
+  // console.log(req.query);
+  // console.log(req.query.search);
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  res.send({
+    product: [],
+  });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", {
-        title: "about me",
-        name: "ALmamad",
-    });
+  res.render("about", {
+    title: "about me",
+    name: "ALmamad",
+  });
 });
 
 app.get("/help", (req, res) => {
-    res.render("help", {
-        helptext: "you can get help mf ",
-    });
+  res.render("help", {
+    title: "help ",
+    name: "mamad ",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    errorMessage: "help Page not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    errorMessage: "404 Page not found :)",
+  });
 });
 
 // app.get("", (req, res) => {
@@ -49,5 +126,5 @@ app.get("/help", (req, res) => {
 // });
 
 app.listen(3000, () => {
-    console.log("Server is UP on port 3000");
+  console.log("Server is UP on port 3000");
 });
