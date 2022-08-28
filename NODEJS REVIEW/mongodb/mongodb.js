5cd67359e05f4430142a763a2645ddcf3f3e252a
mamad <mohammadtt77@yahoo.com>
2022-08-29 00:40:22 +0430

    object ids

--- NODEJS REVIEW/mongodb/mongodb.js
+++ NODEJS REVIEW/mongodb/mongodb.js
@@ -1,9 +1,20 @@
 //CRUD OP
 const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = mongodb;
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.id);
+console.log(id.id.length);
+console.log(id.toHexString());
+console.log(id.toHexString().length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -14,18 +25,60 @@ MongoClient.connect(
     }
     console.log("Connect Correctly");
     const db = client.db(databaseName);
-    db.collection("users").insertOne(
-      {
-        name: "mamad",
-        age: 24,
-      },
-      (error, result) => {
-        console.log("sex");
-        if (error) {
-          return console.log("Unable to insert user");
-        }
-        console.log(result);
-      }
-    );
+    /////////INSERT ONE
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "mamad2",
+    //     age: 69,
+    //   },
+    //   (error, result) => {
+    //     console.log("sex");
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+    //     console.log(result);
+    //   }
+    // );
+    /////////INSERT MANY
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "alexis texas",
+    //       age: 85,
+    //     },
+    //     {
+    //       name: "johnny sins",
+    //       age: "32",
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert many");
+    //     }
+    //     console.log(result);
+    //   }
+    // );
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "doing ass",
+    //       completed: true,
+    //     },
+    //     {
+    //       description: "lick pussy",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "suck dick",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Cant insert many");
+    //     }
+    //     console.log(result);
+    //   }
+    // );
   }
 );
